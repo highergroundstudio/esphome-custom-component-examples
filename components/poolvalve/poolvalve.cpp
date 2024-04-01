89684e3b9d6f67f0e9df5768f59819755a395dc4
Kyle King <highergroundstudio@users.noreply.github.com>
2024-04-01 07:05:24 -0700

    Fix poolvalve module naming inconsistency

--- components/poolvalve/poolvalve.cpp
+++ components/poolvalve/poolvalve.cpp
@@ -1,22 +1,27 @@
-#include "pool_valve.h"
+#include "poolvalve.h"
 #include "esphome/core/log.h"
 
-namespace esphome {
-namespace poolvalve {
+namespace esphome
+{
+  namespace poolvalve
+  {
 
-void PoolValve::change_direction(bool direction) {
-  // Change the valve direction logic here
-  
-  current_direction_ = direction;
-  if (direction_sensor_ != nullptr) {
-    direction_sensor_->publish_state(current_direction_ ? "Direction 1" : "Direction 2");
-  }
-}
+    void PoolValve::change_direction(bool direction)
+    {
+      // Change the valve direction logic here
 
-void PoolValve::on_direction_change_complete() {
-  ESP_LOGD(TAG, "Valve direction change complete.");
-  // Additional actions once the direction change is completed
-}
+      current_direction_ = direction;
+      if (direction_sensor_ != nullptr)
+      {
+        direction_sensor_->publish_state(current_direction_ ? "Direction 1" : "Direction 2");
+      }
+    }
 
-}  // namespace poolvalve
-}  // namespace esphome
+    void PoolValve::on_direction_change_complete()
+    {
+      ESP_LOGD(TAG, "Valve direction change complete.");
+      // Additional actions once the direction change is completed
+    }
+
+  } // namespace poolvalve
+} // namespace esphome
